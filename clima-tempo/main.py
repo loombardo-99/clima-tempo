ef00e4eb444e32f684713febc6895d690bc6deee
Fernando Lombardo Junior <133462875+loombardo-99@users.noreply.github.com>
2024-04-02 15:07:35 -0300

    Add files via upload

---
+++ clima-tempo/main.py
@@ -0,0 +1,44 @@
+
+import requests
+def obter_previsao_tempo(API_KEY, cidade, estado):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+
+    params = {
+        'q': f"{cidade},{estado},Brasil",
+        'appid': API_KEY,
+        'units': 'metric'  # Unidade Celsius
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()  # Lança uma exceção para erros HTTP
+        dados_previsao = response.json()
+        return dados_previsao
+    except requests.exceptions.RequestException as e:
+        print(f"Erro na requisição: {e}")
+        return None
+
+
+def exibir_previsao_tempo(dados_previsao):
+    if dados_previsao is not None:
+        temperatura = dados_previsao['main']['temp']
+        descricao = dados_previsao['weather'][0]['description']
+        print(f"Temperatura: {temperatura}°C")
+        print(f"Condição: {descricao}")
+    else:
+        print("Não foi possível obter a previsão do tempo.")
+
+
+def main():
+    api_key = "2db0dd62cbc23da4ad36140166693cfc"
+    cidade = input("Digite o nome da cidade: ")
+    estado = input("Digite a sigla do estado: ")
+
+    dados_previsao = obter_previsao_tempo(api_key, cidade, estado)
+
+    exibir_previsao_tempo(dados_previsao)
+
+
+if __name__ == "__main__":
+    main()
+
